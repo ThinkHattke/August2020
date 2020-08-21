3db126e26ed7d22cdf2f447ff54d68bc51fd89c9
Gaurav <gauravrai568@gmail.com>
2020-08-21 20:32:02 +0530

    Download an image using Picasso Library

--- app/src/main/java/com/gaurav/august2020/Wallpaper.java
+++ app/src/main/java/com/gaurav/august2020/Wallpaper.java
@@ -9,6 +9,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
 import java.io.InputStream;
 import java.net.URL;
 
@@ -25,38 +27,8 @@ public class Wallpaper extends AppCompatActivity {
 
         String url = "https://cdn.vox-cdn.com/thumbor/iS9cu8-U-hh8rUGhUnc4ulkJ1wE=/0x0:2040x1360/1820x1213/filters:focal(860x1034:1186x1360)/cdn.vox-cdn.com/uploads/chorus_image/image/59377089/wjoel_180413_1777_android_001.1523625143.jpg";
 
-        new DownloadImage(wallpaper_image).execute(url);
+        Picasso.get().load(url).into(wallpaper_image);
 
     }
-
-    class DownloadImage extends AsyncTask<String, Void, Bitmap> {
-
-        ImageView wallpaper_image;
-
-        DownloadImage(ImageView wallpaper_image) {
-            this.wallpaper_image = wallpaper_image;
-        }
-
-        @Override
-        protected Bitmap doInBackground(String... strings) {
-            String url = strings[0];
-            Bitmap bitmap = null;
-
-            try {
-                InputStream inputStream = new URL(url).openStream();
-                bitmap = BitmapFactory.decodeStream(inputStream);
-            } catch (Exception e) {
-
-            }
-
-            return bitmap;
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            super.onPostExecute(bitmap);
-            wallpaper_image.setImageBitmap(bitmap);
-        }
-    }
-
+    
 }
